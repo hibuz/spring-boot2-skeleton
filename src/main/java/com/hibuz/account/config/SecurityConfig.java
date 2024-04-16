6799b5ecbe678d9fca56d1d82c14f54d8fc2d90b
hibuz <hibuz@hibuz.com>
2024-04-17 07:47:48 +0900

    ✨ support spring security

---
+++ src/main/java/com/hibuz/account/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hibuz.account.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    @ConditionalOnProperty(name="spring.h2.console.enabled", havingValue="true")
+    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize ->
+                authorize.antMatchers( "/swagger-ui.html", "/webjars/**", "/swagger-resources/**", "/v2/api-docs").permitAll()
+                        .anyRequest().authenticated());
+
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+}
