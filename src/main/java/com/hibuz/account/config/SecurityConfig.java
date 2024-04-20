efa2276c8c9fa61bb9bb71f993655011a11aab8e
hibuz <hibuz@hibuz.com>
2024-04-20 20:49:26 +0900

    ✨ change swagger2 to springdoc

--- src/main/java/com/hibuz/account/config/SecurityConfig.java
+++ src/main/java/com/hibuz/account/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.hibuz.account.config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,16 +10,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     @Bean
     @ConditionalOnProperty(name="spring.h2.console.enabled", havingValue="true")
     protected SecurityFilterChain config(HttpSecurity http) throws Exception {
+        log.info("http://localhost:8080{}/swagger-ui/index.html", contextPath);
+
 
         http.authorizeHttpRequests(authorize ->
-                authorize.antMatchers( "/swagger-ui.html", "/webjars/**", "/swagger-resources/**", "/v2/api-docs").permitAll()
+                authorize.antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**").permitAll()
                         .antMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated());
 
